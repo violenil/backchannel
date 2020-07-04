3121da9084842a4c74db94164dd390067f5c5b97
Pedro Bascoy <pedrogonzalez.bascoy@usc.es>
2020-07-04 12:33:12 +0200

    Loading datasets from disk is now faster. Batches are selected randomly.

--- ai.py
+++ ai.py
@@ -102,10 +102,10 @@ class conv_net(nn.Module):
         :return: x after the convolution.
         """
         for layer in self.convolutionals:
-            x = F.max_pool2d(F.relu(layer(x)),(1,2))
+            x = F.max_pool2d(F.relu(layer(x)),(1,1))
 
         if self._to_linear is None:
-            print(x[0].shape)
+            print(x.shape)
             self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
 
         return x
@@ -282,8 +282,14 @@ class conv_net(nn.Module):
         # open file to log the accuracies.
         with open(file_name,"a") as f:
             for epoch in range(epochs):
-                for i in tqdm(range(0,len(X_train),batch_size)):
-      # get our batches
+
+                # set how the batches are gonna be forwaded.
+                random_idxs = list(range(0, len(X_train), batch_size))
+
+                random.shuffle(random_idxs)
+
+                for i in tqdm(random_idxs):
+                    # get our batches
                     batch_X = X_train[i:i+batch_size].view(-1,3,self.input_rows,self.input_cols).to(self.device)
 
                     batch_y = y_train[i:i+batch_size].to(self.device)
@@ -305,7 +311,7 @@ class conv_net(nn.Module):
                 losses.append(val_loss)
 
                 # Print accuracies to stdout and log them into the file.
-                print(f"acc: {round(float(acc), 2)}, loss: {round(float(loss), 8)}, val_acc: {round(float(val_acc), 2)}, val_loss: {round(float(val_loss), 8)}")
+                print(f"acc: {round(float(acc), 8)}, loss: {round(float(loss), 8)}, val_acc: {round(float(val_acc), 8)}, val_loss: {round(float(val_loss), 8)}")
                 f.write(
                     f"{file_name},{round(time.time(), 3)},{float(acc)},{float(loss)},{float(val_acc)},{float(val_loss)}\n")
 
